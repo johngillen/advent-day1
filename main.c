42bd0d3405e70119cf528bef377d96ac794539c1
Chad Thundercock <jg@networked.space>
2019-12-01 18:40:56 -0600

    add file input

--- main.c
+++ main.c
@@ -6,7 +6,19 @@ int main(int argc, char const *argv[])
 {
     int sum = 0;
 
-    // input handling
+    FILE * file;
+    file = fopen("input", "r");
+
+    for (;;)
+    {
+        int input;
+        int line = fscanf(file, "%d", &input);
+        printf("%d\n", input);
+        if (line == EOF) { break; }
+        sum += fuel(input);
+    }
+
+    /* only works for typed input
     for (;;)
     {
         int input;
@@ -15,8 +27,9 @@ int main(int argc, char const *argv[])
         if (line == EOF) { break; }
         sum += fuel(input);
     }
+    */
 
-    printf("\nTotal fuel required: %d\n", sum);
+    printf("Total fuel required: %d\n", sum);
     
     return 0;
 }
@@ -26,7 +39,8 @@ Fuel required to launch a given module is based on its mass. Specifically, to
 find the fuel required for a module, take its mass, divide by three, round down,
 and subtract 2.
 */
+
 int fuel(int mass)
 {
-    return floor(mass / 3) - 2;
+    return (int) floor((double) mass / 3) - 2;
 }
